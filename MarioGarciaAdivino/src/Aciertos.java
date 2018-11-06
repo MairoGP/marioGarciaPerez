b2b47fd78b6e95b09bf5532991e3e4e9973631cc
Mario Garcia <mario_onlysk8@hotmail.com>
2018-11-06 17:42:39 +0100

    Cambiamos un comentario

--- MarioGarciaAdivino/src/Aciertos.java
+++ MarioGarciaAdivino/src/Aciertos.java
@@ -5,7 +5,7 @@ public class Aciertos {
     public static void main(String[] args) {
         int n = 0,cont = 0;
         int x = (int) (500 *Math.random());
-        //System.out.print(x);
+        System.out.print(x);
         Scanner sc = new Scanner(System.in);
         do {
             System.out.print("\nIntroduce el valor de X: ");
