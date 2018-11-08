512b9547ad6d3576b694ef1d27d6c24d2482d8a1
Mario Garcia <mario_onlysk8@hotmail.com>
2018-11-08 19:59:37 +0100

    Correcion de errores completo

--- CocheParking/src/com/desarrollos/entde/Car.java
+++ CocheParking/src/com/desarrollos/entde/Car.java
@@ -1,7 +1,4 @@
 package com.desarrollos.entde;
-
-//import java.util.Arrays;
-//import java.awt.Color;
 /**
  * Class that models objects of type "car"
  * 
@@ -29,11 +26,11 @@ public class Car
     public static void main(String[] args) throws InterruptedException{
     	Garage g = new Garage();
     	Car car = new Car(g);
-        car.accelerate();
-        //slowDown();
-        //moveDown();
-        //accelerate();
-        //car.printGarage();
+        Car.accelerate();
+        slowDown();
+        moveDown();
+        accelerate();
+        Garage.printGarage(car);
 //        turnRight();
 //        turnHeadlightsOn();
 //        turnHeadlightsOff();
@@ -43,7 +40,7 @@ public class Car
      * Accelerate the car
      * @throws InterruptedException 
      */
-    public void accelerate() throws InterruptedException{
+    public static void accelerate() throws InterruptedException{
         String c = new String("*");
         String space = new String(" ");
     	for(int i=0; i<5; i++)
@@ -58,12 +55,12 @@ public class Car
      * Slow down the car
      * @throws InterruptedException 
      */
-    public void slowDown() throws InterruptedException{
-    	//string c = new string("     *");
+    public static void slowDown() throws InterruptedException{
+    	String c = new String("     *");
     	for(int i=0; i<5; i++)
         {
-     //  	System.out.print(c);
-     //   	c = c.substring(1);
+       	System.out.print(c);
+        	c = c.substring(1);
         	Thread.sleep(1000);
         }
     	System.out.println();
@@ -73,17 +70,17 @@ public class Car
      * Move the car downwards
      * @throws InterruptedException 
      */    
-    public void moveDown() throws InterruptedException{
-    	//string c = new string("\t\t\t\t  *"));
-    /*	for(int i=0, i<5, i++)
-        {
+    public static void moveDown() throws InterruptedException{
+    	String c = new String("\t\t\t\t  *");
+    	for(int i=0; i<5; i++)
+    	{
     		if(i!=4)
     			System.out.println(c);
     		else 
     			System.out.print(c);
         	Thread.sleep(1000);
         }
-       */
+       
     }
     
 }
