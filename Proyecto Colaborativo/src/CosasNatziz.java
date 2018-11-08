c906776192150c10c91104359ecc41c18186b24a
Mario Garcia <mario_onlysk8@hotmail.com>
2018-11-08 20:16:13 +0100

    Algoritmo para piramide de Floyd

--- Proyecto Colaborativo/src/CosasNatziz.java
+++ Proyecto Colaborativo/src/CosasNatziz.java
@@ -3,7 +3,8 @@ public class CosasNatziz {
 	public static void main (String[] args) {
 		int numero=15;
 		String salida="";
-		
+		//Algoritmo para escalera de numeros
+		/*
 		for (int i = 0; i <= numero; i++) {
 			for(int j=1; j <= i;j++) {
 				salida+=String.format("%5s",j);
@@ -11,5 +12,22 @@ public class CosasNatziz {
 			salida+="\n";
 		}
 		System.out.println(salida);
+		*/
+		
+		//Algoritmo para el triangulo de Floyd
+		
+		int numeroFloyd=5;
+		int contador=1;
+		
+		for(int i=0; i <= numeroFloyd; i++) {
+			for(int j=0; j < i; j++) {
+				salida+=String.format("%5s",contador);
+				contador++;
+			}
+			salida+="\n";
+		}
+		
+		System.out.println(salida);
 	}
+
 }
